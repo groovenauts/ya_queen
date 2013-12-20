14cd132960b4a8ff0071f10d9ba4384109f261e6
akima <akm2000@gmail.com>
2013-12-20 14:39:05 +0900

    add example of basic behavior

--- spec/ya_queen_spec.rb
+++ spec/ya_queen_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
+require 'yaml'
+
 describe YaQueen do
   it 'should have a version number' do
     YaQueen::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should be_true
+  it 'should build tasks' do
+    context = mock(:context)
+    expect(context).to receive(:task).with(:"@api_server")
+
+    %w[api_server01 api_server02].each do |host|
+      expect(context).to receive(:after).with(:"@api_server", :"@api_server/#{host}")
+      expect(context).to receive(:task).with(:"@api_server/#{host}")
+      expect(context).to receive(:after).with(:"@api_server/#{host}", :"@api_server/common")
+    end
+
+    expect(context).to receive(:task).with(:"@api_server/common")
+
+    YaQueen.configure(context, File.expand_path("../test01.yml", __FILE__)) do |c|
+      c.define_server_tasks(:api_server)
+    end
   end
 end
