85406fd0603f0ae5a3172acec088cb98e7e6fa7b
akima <akm2000@gmail.com>
2013-12-23 18:06:57 +0900

    scm と deploy_viaについては値をSymbolに変換するようにしました

--- lib/ya_queen/custom.rb
+++ lib/ya_queen/custom.rb
@@ -5,9 +5,11 @@ module YaQueen
   class Custom < Base
     def implement_common_task
       skip_keys = %w[roles deploy_to]
+      symbol_value_keys = %w[scm deploy_via]
       config.each do |key, value|
         next if key == "roles"
         if value
+          value = symbol_value_keys.include?(key.to_s) ? value.to_sym : value
           set key.to_sym, value
         end
       end
